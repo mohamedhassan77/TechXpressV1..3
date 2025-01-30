ffea936d44551dc331c894bd25b2396ce39a2330
mohamedhassan77 <62241123+mohamedhassan77@users.noreply.github.com>
2025-01-30 21:52:16 +0200

    css

--- TechXpress/wwwroot/js/site.js
+++ TechXpress/wwwroot/js/site.js
@@ -1,4 +1,8 @@
-﻿// Please see documentation at https://learn.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
+﻿document.addEventListener("DOMContentLoaded", function () {
+    const menuToggle = document.getElementById("menu-toggle");
+    const navMenu = document.querySelector(".nav-menu");
 
-// Write your JavaScript code.
+    menuToggle.addEventListener("click", function () {
+        navMenu.classList.toggle("active");
+    });
+});
