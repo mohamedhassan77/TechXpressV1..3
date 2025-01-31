fde2d639de89e107df3c1c84edb5e9494083b2ae
mohamedhassan77 <62241123+mohamedhassan77@users.noreply.github.com>
2025-01-31 10:37:20 +0200

    cs

---
+++ TechXpress/Controllers/CategoryController.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using TechXpress_domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Authorization;
+using TechXpress_context = TechXpress_infrastructure.Data.TechXpress_context;
+
+namespace TechXpress.Controllers
+{
+    [Authorize(Roles = "Admin")]
+    public class CategoryController : Controller
+    {
+        private readonly TechXpress_context _context;
+
+        public CategoryController(TechXpress_context context)
+        {
+            _context = context;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            return View();
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create(Category category)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(category);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(category);
+        }
+
+        // Add Edit, Delete, and Details actions similarly
+    }
+}
---
+++ TechXpress_domain/Entities/Category.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace TechXpress_domain.Entities
+{
+    public class Category
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(100, ErrorMessage = "Category name must be less than 100 characters.")]
+        public string Name { get; set; }
+
+        [StringLength(500, ErrorMessage = "Description must be less than 500 characters.")]
+        public string Description { get; set; }
+
+        // Navigation property for products
+        public ICollection<Product> Products { get; set; }
+    }
+}
